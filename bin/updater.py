d0d0e30c4e89dee25bd6cde0d9eed8d0ae5a7c64
Hashm <pro.32126@gmail.com>
2022-01-30 21:07:55 -0500

    fixed updated (hopefully)

--- bin/updater.py
+++ bin/updater.py
@@ -10,5 +10,5 @@ for i in range(0,33):
 for i in range(len(newcontents)):
     if newcontents[i] == '<a href="https://github.com/imfunniee" target="_blank">made on earth by a human</a>':
         newcontents = '<a>made with </a><a href="https://github.com/imfunniee/gitfolio" target="_blank">gitfolio</a><a> and modified by </a><a href="https://github.com/imfunniee/gitfolio">me</a>'
-with open("index.html", "w") as w:
+with open("/projects/hashmweb/site/dist/index.html", "w") as w:
     w.writelines(newcontents)
