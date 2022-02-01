9ba4bca6ca8c7226180b86ed7523c6f1bc0c64ad
Hashm <pro.32126@gmail.com>
2022-01-31 20:49:08 -0500

    fix overlapping names causing issues :(

--- bin/update-script.sh
+++ bin/update-script.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-PID=`ps -ef | grep hashmweb | grep -v grep | awk '{print $2}'`
+PID=`ps -ef | grep hashmtech | grep -v grep | awk '{print $2}'`
 kill $PID
 /projects/hashmweb/bin/uweb &
 python3 /projects/hashmweb/bin/updater.py
 PID=`ps -ef | grep uweb | grep -v grep | awk '{print $2}'`
 kill $PID
-/projects/hashmweb/bin/hashmweb &
+/projects/hashmweb/bin/hashmtech &
