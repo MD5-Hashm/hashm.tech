d3ac472ab82125126aab5a7e1027ca8227ba1219
Hashm <pro.32126@gmail.com>
2022-02-01 17:50:37 -0500

    fix updater (finally)

--- bin/updater.py
+++ bin/updater.py
@@ -1,14 +1,14 @@
 import os
 os.chdir("/projects/hashmweb/site/")
 os.system("gitfolio update")
-with open("/projects/hashmweb/site/dist/index.html", "r") as r:
+with open("index.html", "r") as r:
     newcontents = r.readlines()
-with open("/projects/hashmweb/old/oldindex.html", "r") as r:
+with open("C:\\Users\\pro32\\Desktop\\hashm.tech\\site\\dist\\index.html", "r") as r:
     oldcontents = r.readlines()
-for i in range(0,33):
+for i in range(0,38):
     newcontents[i] = oldcontents[i]
-for i in range(len(newcontents)):
-    if newcontents[i] == '<a href="https://github.com/imfunniee" target="_blank">made on earth by a human</a>':
-        newcontents = '<a>made with </a><a href="https://github.com/imfunniee/gitfolio" target="_blank">gitfolio</a><a> and modified by </a><a href="https://github.com/imfunniee/gitfolio">me</a>'
-with open("/projects/hashmweb/site/dist/index.html", "w") as w:
+for i in range(0,len(newcontents)):
+    if '        <a href="https://github.com/imfunniee" target="_blank">made on earth by a human</a>' in newcontents[i]:
+        newcontents[i] = '        <a>made with </a><a href="https://github.com/imfunniee/gitfolio" target="_blank">gitfolio</a><a> and modified by </a><a href="https://github.com/MD5-Hashm/hashm.tech">me</a>'
+with open("indexout.html", "w") as w:
     w.writelines(newcontents)
