3351b6bf1403fb9309b992691dc1c80a4e42c915
Hashm <pro.32126@gmail.com>
2022-02-04 13:26:30 -0500

    remove slope

--- bin/hashmtech.go
+++ bin/hashmtech.go
@@ -8,7 +8,6 @@ import (
 
 func serve() {
 	http.Handle("/", http.FileServer(http.Dir("/projects/hashmweb/site/dist/")))
-	http.Handle("/slope", http.FileServer(http.Dir("/projects/hashmweb/site/slope/slope-game/")))
 	http.ListenAndServe(":80", nil)
 }
 
